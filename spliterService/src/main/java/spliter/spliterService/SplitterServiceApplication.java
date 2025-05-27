02f41468dfd83a632047148a027b3ee3dc5b7a6e
SebaJuarez <113382513+SebaJuarez@users.noreply.github.com>
2025-05-27 16:30:39 -0300

    feat(services): add splitter service and implement actuator endpoints
    - Add splitter service to the project
    - Implement Actuator endpoints for health, info, and metrics
    - Update Docker Compose configuration to include new service
    - Add Dockerfile for Spring Boot application
    - Create new Maven project for splitter service
    - Configure application properties for splitter service

---
+++ spliterService/src/main/java/spliter/spliterService/SplitterServiceApplication.java
@@ -0,0 +1,13 @@
+package spliter.spliterService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitterServiceApplication.class, args);
+	}
+
+}
---
+++ spliterService/src/test/java/spliter/spliterService/SplitterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spliter.spliterService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
