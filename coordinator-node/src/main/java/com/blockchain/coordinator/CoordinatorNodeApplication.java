11776db9e0478f035e18eb71c2bb69672a869c60
SebaJuarez <113382513+SebaJuarez@users.noreply.github.com>
2025-06-17 12:36:32 -0300

    chore: add .gitignore

---
+++ coordinator-node/src/main/java/com/blockchain/coordinator/CoordinatorNodeApplication.java
@@ -0,0 +1,13 @@
+package com.blockchain.coordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinatorNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinatorNodeApplication.class, args);
+	}
+
+}
---
+++ coordinator-node/src/test/java/com/blockchain/coordinator/CoordinatorNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockchain.coordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinatorNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
