6f80115e455583607738f2f154121ef9f7d45689
Depa <depa.agustin@gmail.com>
2025-06-24 15:47:47 -0300

    fix(deployment): correccion.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 kubectl apply -f deploy/namespaces/
+kubectl apply -f deploy/redis-rabbit/
 kubectl apply -f deploy/secrets/
 kubectl apply -f deploy/serviceAccount/
 kubectl apply -f deploy/services/
