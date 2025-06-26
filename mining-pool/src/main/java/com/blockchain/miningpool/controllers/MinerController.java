ac5520b71d0b78e3e0c4743dbe1f50e7767600c8
SebaJuarez <113382513+SebaJuarez@users.noreply.github.com>
2025-06-26 00:25:47 -0300

    feat: Add cross-origin support for miner controller
    - Add @CrossOrigin("*") annotation to MinerController
    - Enable cross-origin requests from any domain

--- mining-pool/src/main/java/com/blockchain/miningpool/controllers/MinerController.java
+++ mining-pool/src/main/java/com/blockchain/miningpool/controllers/MinerController.java
@@ -24,6 +24,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 @RequestMapping("/api/pools")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class MinerController {
 
     private final MinerService minerService;
