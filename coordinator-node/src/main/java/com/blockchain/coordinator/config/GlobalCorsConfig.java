04be8060b25d9baaea7ce6f4a2fdd53b524a34b8
SebaJuarez <113382513+SebaJuarez@users.noreply.github.com>
2026-01-18 19:49:41 -0300

    config: enhance CORS configuration for coordinator-node and mining-pool

--- coordinator-node/src/main/java/com/blockchain/coordinator/config/GlobalCorsConfig.java
+++ coordinator-node/src/main/java/com/blockchain/coordinator/config/GlobalCorsConfig.java
@@ -2,22 +2,27 @@ package com.blockchain.coordinator.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import java.util.Collections;
 
 @Configuration
 public class GlobalCorsConfig {
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public CorsFilter corsFilter() {
+        System.out.println("CorsFilter");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         
-        config.addAllowedOriginPattern("*"); 
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
         config.setAllowCredentials(false);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
         
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
