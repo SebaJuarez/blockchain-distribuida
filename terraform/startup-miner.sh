2d7b5410e0eeee5ed2e1c6a0ab9271b2bdd628e6
SebaJuarez <113382513+SebaJuarez@users.noreply.github.com>
2026-01-29 18:03:23 -0300

    Update startup-miner.sh

--- terraform/startup-miner.sh
+++ terraform/startup-miner.sh
@@ -4,22 +4,24 @@ set -x
 
 # === Startup Worker Sobel ===
 
+RABBIT_HOST="rabbitmq.internal"
+RABBIT_PORT="5672"
+
 # 1) Actualizo repositorios e instalo dependencias
 sudo apt-get update -y
 sudo apt-get install -y docker.io netcat-traditional
 
-# 2) Espero a que RabbitMQ este disponible
-until nc -z 35.239.206.202 5672; do
+# 2) Espero a que RabbitMQ esté disponible
+until nc -z $RABBIT_HOST $RABBIT_PORT; do
   sleep 5
 done
 
-
 # 3) Arranco el contenedor de worker
 sudo docker run --rm \
-  -e RABBITMQ_HOST=35.239.206.202 \
+  -e RABBITMQ_HOST=$RABBIT_HOST \
   -e RABBITMQ_USER=guest \
   -e RABBITMQ_PASS=guest \
-  -e RABBITMQ_PORT=5672 \
+  -e RABBITMQ_PORT=$RABBIT_PORT \
   dagyss/minero-registrable-cpu:latest
 
 # 4) Fin del script
